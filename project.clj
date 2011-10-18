beff0719a6a83c53a87776103ddfa1ebdcbc7177
Nick Drew <nick.s.drew@gmail.com>
2011-10-18 18:21:47 +1100

    Added overtone dependencies

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject overtone-demo "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [
+	[org.clojure/clojure "1.2.1"]
+	[overtone "0.4.0"]
+	])
